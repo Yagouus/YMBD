0c8cf97ce1b2ddc2bdd54cb11017651272cf1666
Yagouus <yagofontenla95@gmail.com>
2016-04-16 17:45:28 +0200

    Filter in PeliculasProximamentCtrl.js

--- scripts/controllers/PeliculasProximamenteCtrl.js
+++ scripts/controllers/PeliculasProximamenteCtrl.js
@@ -1,10 +1,10 @@
-angular.module("misPelisSeriesApp").controller("PeliculasProximamenteCtrl", ["$scope", "ApiService" ,function ($scope, ApiService){
+angular.module("misPelisSeriesApp").controller("PeliculasProximamenteCtrl", ["$scope", "ApiService" ,"$filter" ,function ($scope, ApiService, $filter){
 
     ApiService
         .consultaApi("movie/upcoming")
         .then(
             function(resultado){
-                $scope.peliculas = resultado.data.results;
+                $scope.peliculas = $filter("orderBy")(resultado.data.results, "release_date");
             },
             function () {
                 alert("Algo no ha ido bien.");
