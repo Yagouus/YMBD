6681e5e8794b1bd20839656ec2040a65643eb32e
Yagouus <yagofontenla95@gmail.com>
2016-04-18 23:23:45 +0200

    route otherwise added

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,14 @@
 angular.module("misPelisSeriesApp", ["ngRoute", "route-segment", "view-segment"]);
 
 //Definimos secciones
-  angular.module("misPelisSeriesApp").config(["$routeSegmentProvider", function ($routeSegmentProvider) {
-      $routeSegmentProvider.when("/peliculas", "peliculas");
+  angular.module("misPelisSeriesApp").config(["$routeSegmentProvider", "$routeProvider", function ($routeSegmentProvider, $routeProvider) {
+      $routeSegmentProvider.when("/peliculas", "peliculas.proximamente");
       $routeSegmentProvider.when("/peliculas/proximamente", "peliculas.proximamente");
       $routeSegmentProvider.when("/peliculas/cartelera", "peliculas.cartelera");
       $routeSegmentProvider.when("/series", "series");
+      //$routeSegmentProvider.when("/series/hoy", "series.hoy");
+      //$routeSegmentProvider.when("/series/emision", "series.emision");
+      $routeProvider.otherwise("/peliculas/proximamente");
       
       //Peliculas
       $routeSegmentProvider.segment("peliculas", {
